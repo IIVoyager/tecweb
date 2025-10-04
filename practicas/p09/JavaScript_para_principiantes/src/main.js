62a9a21565f2e270c888b64b100209c1eb388755
IIVoyager <ac5151832@gmail.com>
2025-10-04 17:43:49 -0600

    Función de ejemplo 1 "Hola mundo"

--- practicas/p09/JavaScript_para_principiantes/src/main.js
+++ practicas/p09/JavaScript_para_principiantes/src/main.js
@@ -1,13 +1,15 @@
-
-function getDatos()
-{
+// Función de ejemplo
+function getDatos() {
     var nombre = prompt("Nombre: ", "");
-
     var edad = prompt("Edad: ", 0);
-
     var div1 = document.getElementById('nombre');
     div1.innerHTML = '<h3> Nombre: '+nombre+'</h3>';
-
     var div2 = document.getElementById('edad');
     div2.innerHTML = '<h3> Edad: '+edad+'</h3>';
+}
+
+// Ejemplo 1: Hola mundo con document.write()
+function ejemplo1() {
+    document.getElementById('resultado1').innerHTML = '';
+    document.write("Hola Mundo");
 }

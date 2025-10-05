36f9ac335e363bdc08a112c88e2c9eaa8346ae9a
IIVoyager <ac5151832@gmail.com>
2025-10-04 18:05:16 -0600

    Función de ejemplo 2 "Mostrar variables"

--- practicas/p09/JavaScript_para_principiantes/src/main.js
+++ practicas/p09/JavaScript_para_principiantes/src/main.js
@@ -13,3 +13,18 @@ function ejemplo1() {
     document.getElementById('resultado1').innerHTML = '';
     document.write("Hola Mundo");
 }
+
+// Ejemplo 2: Mostrar variables
+function mostrarVariables() { 
+    // Definir las variables
+    var nombre = 'Juan';
+    var edad = 10;
+    var altura = 1.92;
+    var casado = false;
+    
+    document.getElementById('resultadoVariables').innerHTML = '';
+    document.write('<h3>Nombre: '+nombre+'</h3>');
+    document.write('<h3>Edad: '+edad+'</h3>');
+    document.write('<h3>Altura: '+altura+'</h3>');
+    document.write('<h3>Casado: '+casado+'</h3>');
+}

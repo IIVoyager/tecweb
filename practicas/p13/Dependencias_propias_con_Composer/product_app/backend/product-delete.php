1830dc1c02bb0c78cd4c80b70a5888e9c9f629fb
IIVoyager <ac5151832@gmail.com>
2025-11-22 16:13:28 -0600

    Namespaces organizados

---
+++ practicas/p13/Dependencias_propias_con_Composer/product_app/backend/product-delete.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Tecweb\MyApi\Delete\Delete;
+
+$productos = new Delete('marketzone');
+$productos->delete($_POST['id']);
+echo $productos->getData();
+?>
---
+++ practicas/p13/Dependencias_propias_con_Composer/product_app/backend/product-list.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Tecweb\MyApi\Read\Read;
+
+$productos = new Read('marketzone');
+$productos->list();
+echo $productos->getData();
+?>

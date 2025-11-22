5ea56e8d85e5ff575638b4a35fd2314bff009bbf
IIVoyager <ac5151832@gmail.com>
2025-11-22 16:14:16 -0600

    Archivos autogenerados por Composer

---
+++ practicas/p13/Dependencias_propias_con_Composer/product_app/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tecweb/productapp',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '38bcd2762704b413c66933eb9f4a88e673aad9ee',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tecweb/productapp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '38bcd2762704b413c66933eb9f4a88e673aad9ee',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
